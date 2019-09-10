33f8f7fafab327bb3b1c1eb22dcd0c1b33da2668
marwan-at-work <marwan.sameer@gmail.com>
2019-09-10 12:10:56 -0400

    1

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/gomvs/f
+
+go 1.13
+
+require github.com/gomvs/g v1.1.0 // indirect
---
+++ hello.go
@@ -0,0 +1,12 @@
+package f
+
+import (
+	"fmt"
+
+	"github.com/gomvs/g"
+)
+
+func Hello() {
+	fmt.Println("F v1.1.0")
+	g.Hello()
+}
